0557a85ef32080225063bfe392832b821ab83628
Raushanray <raushankuray1998@gmail.com>
2023-07-04 13:05:39 +0530

    create role base authentication and add three roles Employee, manager, and admin, here employee can access only get method and manager can access get getall, post and put method and finally admin can access all get , getall, post, put and delete also..

--- 00-spring-boot-rest-security-employee-starter-code/src/main/java/com/codewithraushan/springboot/cruddemo/security/DemoSecurityConfig.java
+++ 00-spring-boot-rest-security-employee-starter-code/src/main/java/com/codewithraushan/springboot/cruddemo/security/DemoSecurityConfig.java
@@ -2,9 +2,13 @@ package com.codewithraushan.springboot.cruddemo.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class DemoSecurityConfig {
@@ -25,13 +29,34 @@ public class DemoSecurityConfig {
                 .build();
 
 
-        UserDetails Ayan = User.builder()
-                .username("Ayan")
+        UserDetails Raushan = User.builder()
+                .username("Raushan")
                 .password("{noop}test123")
                 .roles("EMPLOYEE","MANAGER", "ADMIN")
                 .build();
 
-        return new InMemoryUserDetailsManager(Ravi, Sumit, Ayan);
+        return new InMemoryUserDetailsManager(Ravi, Sumit, Raushan);
 
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception{
+        httpSecurity.authorizeHttpRequests(configure ->
+                configure
+                        .requestMatchers(HttpMethod.GET,"/api/employees").hasRole("EMPLOYEE")
+                        .requestMatchers(HttpMethod.GET,"/api/employees/**").hasRole("EMPLOYEE")
+                        .requestMatchers(HttpMethod.POST,"/api/employees").hasRole("MANAGER")
+                        .requestMatchers(HttpMethod.PUT,"/api/employees").hasRole("MANAGER")
+                        .requestMatchers(HttpMethod.DELETE,"/api/employees/**").hasRole("ADMIN")
+        );
+
+        //use basic Authentication
+        httpSecurity.httpBasic(Customizer.withDefaults());
+
+        //disable Cross-Site Request Forgery (CSRF)
+        //in general not request for stateless Rest apis that use POST, PUT, DELETE and/or PATCH
+        httpSecurity.csrf(csrf -> csrf.disable());
+
+        return httpSecurity.build();
     }
 }
